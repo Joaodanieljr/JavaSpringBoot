e592f0acde2b8e16b0e8554fdee2a89fbc3db7f4
joaodanieljr <joaodanieljr@gmail.com>
2020-12-02 23:09:05 -0300

    Projeto Criado

---
+++ src/main/java/com/joaodanieljr/projetoLojaJava/ProjetoLojaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.joaodanieljr.projetoLojaJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLojaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLojaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaodanieljr/projetoLojaJava/ProjetoLojaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodanieljr.projetoLojaJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLojaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
