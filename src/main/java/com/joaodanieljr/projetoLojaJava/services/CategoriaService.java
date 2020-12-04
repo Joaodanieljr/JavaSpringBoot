2bf4ccf0d47b36e39f4bc02ef5564e24c0c6cabc
joaodanieljr <joaodanieljr@gmail.com>
2020-12-03 22:52:38 -0300

    Repository e Service para Categoria

---
+++ src/main/java/com/joaodanieljr/projetoLojaJava/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.joaodanieljr.projetoLojaJava.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaodanieljr.projetoLojaJava.domain.Categoria;
+import com.joaodanieljr.projetoLojaJava.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null); 
+
+	}
+}
