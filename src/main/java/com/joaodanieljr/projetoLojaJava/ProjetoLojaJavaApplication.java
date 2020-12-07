e60a7b6a9e95bf7ced1d4327bdfc17fe3236a124
joaodanieljr <joaodanieljr@gmail.com>
2020-12-06 23:20:22 -0300

    Produto Criado

--- src/main/java/com/joaodanieljr/projetoLojaJava/ProjetoLojaJavaApplication.java
+++ src/main/java/com/joaodanieljr/projetoLojaJava/ProjetoLojaJavaApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.joaodanieljr.projetoLojaJava.domain.Categoria;
+import com.joaodanieljr.projetoLojaJava.domain.Produto;
 import com.joaodanieljr.projetoLojaJava.repositories.CategoriaRepository;
+import com.joaodanieljr.projetoLojaJava.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class ProjetoLojaJavaApplication implements CommandLineRunner {
+public class ProjetoLojaJavaApplication implements CommandLineRunner  { 
+	// command serve para rodar algo antes da app inicializar.
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoLojaJavaApplication.class, args);
@@ -25,7 +31,20 @@ public class ProjetoLojaJavaApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
