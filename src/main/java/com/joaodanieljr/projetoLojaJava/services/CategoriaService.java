2a04d9c7846fb8dac87bb660a2c046a5307a4c9b
joaodanieljr <joaodanieljr@gmail.com>
2020-12-09 23:07:22 -0300

    Ajuste endPoint categoria/id

--- src/main/java/com/joaodanieljr/projetoLojaJava/services/CategoriaService.java
+++ src/main/java/com/joaodanieljr/projetoLojaJava/services/CategoriaService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaodanieljr.projetoLojaJava.domain.Categoria;
+import com.joaodanieljr.projetoLojaJava.exceptions.ObjectNotFoundException;
 import com.joaodanieljr.projetoLojaJava.repositories.CategoriaRepository;
+
 
 @Service
 public class CategoriaService {
@@ -16,7 +18,13 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+																"Objeto não encontrado! ID: " 
+																+ id 
+																+ ", Tipo: " 
+																+ Categoria.class.getName())); 
+		
+		
 
 	}
 }
