da0f78baa661342edd5b29c6be78e1200b109ebf
joaodanieljr <joaodanieljr@gmail.com>
2020-12-17 23:11:49 -0300

    Endpoint clientes/{id}

---
+++ src/main/java/com/joaodanieljr/projetoLojaJava/services/ClienteService.java
@@ -0,0 +1,30 @@
+package com.joaodanieljr.projetoLojaJava.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaodanieljr.projetoLojaJava.domain.Cliente;
+import com.joaodanieljr.projetoLojaJava.exceptions.ObjectNotFoundException;
+import com.joaodanieljr.projetoLojaJava.repositories.ClienteRepository;
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+																"Objeto não encontrado! ID: " 
+																+ id 
+																+ ", Tipo: " 
+																+ Cliente.class.getName())); 
+		
+		
+
+	}
+}
