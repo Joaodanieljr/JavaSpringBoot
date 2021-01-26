151a5bf15b4005a8ba9451dd99673df3e21f6491
joaodanieljr <joaodanieljr@gmail.com>
2021-01-26 00:26:59 -0300

    Ajustes Finais

--- src/main/java/com/joaodanieljr/projetoLojaJava/ProjetoLojaJavaApplication.java
+++ src/main/java/com/joaodanieljr/projetoLojaJava/ProjetoLojaJavaApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjetoLojaJavaApplication implements CommandLineRunner  { 
 
+	//Projeto espelho das aulas do curso de Spring - JAVA - Udemy - 26/01/2021 - 00:26 - BackEnd Finalizado
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoLojaJavaApplication.class, args);
 	}
